38e9f77caa4ddaddcda4642f635e5eb9d90d7ae2
Todor Todorov <todor@peychev.com>
2015-01-21 11:55:47 +0100

    fix examples after package and classname changes

--- examples/project/plugins.sbt
+++ examples/project/plugins.sbt
@@ -1,1 +1,1 @@
-addSbtPlugin("com.voxsupplychain" %% "json-schema-codegen-sbt" % "0.0.2")
+addSbtPlugin("com.voxsupplychain" %% "json-schema-codegen-sbt" % "0.0.3")
--- examples/src/test/scala/ExamplesTest.scala
+++ examples/src/test/scala/ExamplesTest.scala
@@ -11,32 +11,32 @@ class ExamplesTest extends FlatSpec with Matchers {
 
 
   "AdditionalPropertiesJson" should "encode and decode" in {
-    import additional.properties.Codecs._
-    test(additional.properties.AdditionalProperties("bvalue", Some(Map("p1" -> additional.properties.AdditionalPropertiesAdditional(1)))))
+    import additional.PropertiesCodec._
+    test(additional.Properties("bvalue", Some(Map("p1" -> additional.PropertiesAdditional(1)))))
   }
 
   "AdditionalPropertiesOnlyJson" should "encode and decode" in {
-    import additional.properties.only.Codecs._
-    test(additional.properties.only.AdditionalPropertiesOnly(Some(Map("p1" -> additional.properties.only.AdditionalPropertiesOnlyAdditional(1)))))
+    import additional.properties.OnlyCodec._
+    test(additional.properties.Only(Some(Map("p1" -> additional.properties.OnlyAdditional(1)))))
   }
 
   "EnumStrings" should "encode and decode" in {
     import strings.Strings
-    import strings.Codecs._
+    import strings.StringsCodec._
     test(Strings.a)
     test(Strings.b)
   }
 
   "EnumIntegers" should "encode and decode" in {
     import integers.Integers
-    import integers.Codecs._
+    import integers.IntegersCodec._
     test(Integers.v10)
     test(Integers.v20)
   }
 
   "Formats" should "encode and decode" in {
     import formats.Formats
-    import formats.Codecs._
+    import formats.FormatsCodec._
     test(Formats(
       new URI("http://uri/address"),
       InetAddress.getByName("127.0.0.1").asInstanceOf[Inet4Address],
@@ -46,7 +46,7 @@ class ExamplesTest extends FlatSpec with Matchers {
   }
 
   "Product" should "decode from string and encode to string" in {
-    import product.Codecs._
+    import product.ProductCodec._
     val js = """{"name":"Recharge Cards (5 PIN)","prices":[{"cost":0.0187,"currency":"USD","moq":200000}],"eid":"iso-card-5-pin","description":"<p>ISO card, 5 PINs, printed 4 colour front and back</p>\n<p>Every card option shown below meets Tier 1 operator quality standards, at a competitive pricing including freight to your country that’s always openly visible, with streamlined fulfillment and support included, creating what we believe is the best overall value at the lowest total cost of ownership in the industry.</p>\n<p>Material:        Cardboard 300 GSM, UV varnish both sides</p>\n<p>Scratch panel:   Silver/Black Ink with black overprint</p> \n<p>Individually plastic wrapped in chain of 50 cards</p>\n<p>Small boxes of 500 cards, Master Carton of 5000 cards</p>\n<p>Alternate names: Scratch cards, RCV, top-up cards</p>\n","properties":[{"name":"Overscratch Protection","options":[{"name":"No protection"},{"name":"Protective measures against over scratching","prices":[{"cost":0.0253,"currency":"USD","moq":200000},{"cost":0.021,"currency":"USD","moq":500000},{"cost":0.02,"currency":"USD","moq":1000000},{"cost":0.0188,"currency":"USD","moq":5000000,"leadtime":21},{"cost":0.0173,"currency":"USD","moq":10000000},{"cost":0.0171,"currency":"USD","moq":50000000,"leadtime":28}]}]},{"name":"Payment terms","options":[{"name":"Payment on shipment readiness"},{"name":"Net 30 (subject to approval)"}]},{"name":"Order Timing","options":[{"name":"Ship order when ready"},{"name":"Pre-order for shipment in 3 months"}]}],"client":"112","sample":{"price":{"cost":250,"currency":"USD"}},"category":"recharge_cards","leadtime":14,"imageUrl":["https://d2w2n7dk76p3lq.cloudfront.net/product_image/recharge_cards/iso-5pin.png"],"types":[{"name":"Recharge Cards (5 PIN)","prices":[{"cost":0.0187,"currency":"USD","moq":200000},{"cost":0.0175,"currency":"USD","moq":500000},{"cost":0.0162,"currency":"USD","moq":1000000},{"cost":0.0153,"currency":"USD","moq":5000000,"leadtime":21},{"cost":0.0138,"currency":"USD","moq":10000000,"leadtime":28},{"cost":0.0137,"currency":"USD","moq":50000000,"leadtime":28}]}],"presentation":1000}"""
     val po = js.decodeValidation[Product]
     println(po)
